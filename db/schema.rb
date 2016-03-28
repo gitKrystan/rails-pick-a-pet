6ea591839e25409dabde7b21f1d46b28ff80f84a
Krystan Menne and Mike Mahoney <mahonmr@me.com>
2016-03-28 14:14:44 -0700

    add vote model to app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328174308) do
+ActiveRecord::Schema.define(version: 20160328210835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,14 @@ ActiveRecord::Schema.define(version: 20160328174308) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "answer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["answer_id"], name: "index_votes_on_answer_id", using: :btree
+
   add_foreign_key "answers", "questions"
+  add_foreign_key "votes", "answers"
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should validate_presence_of :content }
+  it { should have_many(:votes).dependent :destroy }
 end
