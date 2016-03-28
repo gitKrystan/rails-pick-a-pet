bec749b5a6b6342afda96e80790c7a2a1b1213af
Krystan Menne and Mike Mahoney <kmenne@gmail.com>
2016-03-28 11:00:50 -0700

    allow questions to accept nested attributes for answers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should accept_nested_attributes_for :answers }
 end
