64afc51a607fa3615834ed17adb07ff45fd616d0
Krystan Menne and Mike Mahoney <kmenne@gmail.com>
2016-03-29 13:32:13 -0700

    fix vote spec and refactor forms

--- spec/features/vote_create_pages_spec.rb
+++ spec/features/vote_create_pages_spec.rb
@@ -5,9 +5,9 @@ describe 'the vote on an answer path' do
   let!(:test_answer_1) { test_question.answers[0] }
   let!(:test_answer_2) { test_question.answers[1] }
 
-  it 'allows a user to vote for an answer without javascript', js: true do
+  it 'allows a user to vote for an answer', js: true do
     visit root_path
-    click_button "vote-answer-#{test_answer_1.id}"
+    click_on test_answer_1.content
     expect(page).to have_content('Thanks for voting!')
     expect(current_path).to eq root_path
   end
