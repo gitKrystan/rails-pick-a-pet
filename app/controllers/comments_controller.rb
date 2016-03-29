c6d4460e4148fd681b5a716030a57633c206b17b
Krystan Menne and Mike Mahoney <mahonmr@me.com>
2016-03-29 10:35:26 -0700

    add flash messages when commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,14 @@ class CommentsController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @comment = @question.comments.new(comment_params)
+    @notice = 'Thanks for commenting!'
     if @comment.save
-      flash[:notice] = 'Thanks for commenting!'
       respond_to do |format|
         format.js
-        format.html { redirect_to questions_path }
+        format.html do
+          flash[:notice] = @notice
+          redirect_to questions_path
+        end
       end
     else
       flash[:alert] = 'Oops. Something went wrong. Call Mike for more info.'
--- spec/features/comment_create_pages_spec.rb
+++ spec/features/comment_create_pages_spec.rb
@@ -10,6 +10,7 @@ describe 'the add a comment path' do
       fill_in 'comment_content', with: test_comment_attributes[:content]
       click_button 'Comment'
     end
+    expect(page).to have_content('Thanks for commenting!')
     expect(page).to have_content(test_comment_attributes[:content])
     expect(current_path).to eq root_path
   end
@@ -20,6 +21,7 @@ describe 'the add a comment path' do
       fill_in 'comment_content', with: test_comment_attributes[:content]
       click_button 'Comment'
     end
+    expect(page).to have_content('Thanks for commenting!')
     expect(page).to have_content(test_comment_attributes[:content])
     expect(current_path).to eq questions_path
   end
